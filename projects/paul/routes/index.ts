81f31a26eb2080baf3c7b089e913f8bad41e2bc6
Pedro Martin <pedro.martin@101ways.com>
2023-05-26 15:36:15 +0100

    Add get packag ename

--- projects/paul/routes/index.ts
+++ projects/paul/routes/index.ts
@@ -1,14 +1,17 @@
 import express, { Request, Response } from 'express';
 import Joi from 'joi';
 
+import { getPackageName } from '../config';
+
 const messageSchema = Joi.object({
   message: Joi.string().min(1).required(),
 });
 
 const rootRouter = express.Router();
 
-rootRouter.get('/', (_request: Request, response: Response) => {
-  response.send('<h1>Paul</h1>');
+rootRouter.get('/', async (_request: Request, response: Response) => {
+  const name: string = await getPackageName();
+  response.send(`<h1>${name.toUpperCase()}</h1>`);
 });
 
 rootRouter.post('/', (request: Request, response: Response) => {
