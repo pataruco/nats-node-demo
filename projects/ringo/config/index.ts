dda1a1e80cb8e2b1a0b2da11816a9a26ece5adc4
Pedro Martin <pedro.martin@101ways.com>
2023-05-27 12:54:08 +0100

    Create multiple servers

---
+++ projects/ringo/config/index.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+import { readFile } from 'node:fs/promises';
+
+dotenv.config();
+
+export const HOST = process.env.HOST || '127.0.0.1';
+export const PORT = process.env.PORT || 0;
+
+const fileContents = await readFile('./package.json', { encoding: 'utf-8' });
+
+export const { name: packageJsonName } = JSON.parse(fileContents);
---
+++ projects/ringo/routes/index.ts
@@ -0,0 +1,51 @@
+import express, { Request, Response } from 'express';
+import Joi from 'joi';
+
+import { BeatlesMessage, natsClient, stringCodec } from 'shared';
+import { packageJsonName } from '../config/index.js';
+
+const messageSchema = Joi.object({
+  singers: Joi.array()
+    .items(
+      Joi.string().valid('paul'),
+      Joi.string().valid('john'),
+      Joi.string().valid('ringo'),
+      Joi.string().valid('george'),
+    )
+    .min(1),
+});
+
+const rootRouter = express.Router();
+
+rootRouter.get('/', async (_request: Request, response: Response) => {
+  response.send(`<h1>${packageJsonName.toUpperCase()}</h1>`);
+});
+
+rootRouter.post('/', (request: Request, response: Response) => {
+  const { body } = request;
+
+  const { error } = messageSchema.validate(body);
+  if (error) {
+    const errors = error.details.map(({ message }) => message);
+
+    return response.status(400).send({ errors: [...errors] });
+  }
+
+  const { singers } = body;
+
+  const singersString = singers.join(', ');
+
+  const payloadToPublish: BeatlesMessage = {
+    from: packageJsonName,
+    to: singers,
+  };
+
+  natsClient.publish(
+    'beatles',
+    stringCodec.encode(JSON.stringify(payloadToPublish)),
+  );
+
+  return response.send({ message: `people singing: ${singersString}` });
+});
+
+export default rootRouter;
