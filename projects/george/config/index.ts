0aa4cdf598a146d0a916e9d9c64c5dfb794698f4
Pedro Martin <pedro.martin@101ways.com>
2023-05-27 14:03:27 +0100

    Create docker manifest for george service

--- projects/george/config/index.ts
+++ projects/george/config/index.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 export const HOST = process.env.HOST || '127.0.0.1';
 export const PORT = process.env.PORT || 0;
+export const NATS_SERVER_URL = process.env.NATS_SERVER_URL || '0.0.0.0:4222';
 
 const fileContents = await readFile('./package.json', { encoding: 'utf-8' });
 
